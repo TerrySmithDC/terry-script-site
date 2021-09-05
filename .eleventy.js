4905aa53f3a380b319a3dca1c215b60c00021e79
Terry Smith <1530868+TerrySmithDC@users.noreply.github.com>
2021-09-04 23:11:42 -0300

    naive cacheing + move back to dist folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = (config) => {
   config.addPassthroughCopy({ public: "./" });
   config.setBrowserSyncConfig({
-    files: ["docs/**/*"],
+    files: ["dist/**/*"],
     open: true,
     // Tweak for Turbolinks & Browserstack Compatibility
     snippetOptions: {
@@ -15,10 +15,17 @@ module.exports = (config) => {
   });
   config.setDataDeepMerge(true);
 
+  config.addFilter("bust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", Date.now());
+    return `${urlPart}?${params}`;
+  });
+
   return {
     dir: {
       input: "src",
-      output: "docs",
+      output: "dist",
     },
   };
 };
