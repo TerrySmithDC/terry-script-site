cb809e247b98b6f2ccdb6b84b98c0b15bffd5ba8
Terry Smith <1530868+TerrySmithDC@users.noreply.github.com>
2021-09-05 19:02:16 -0300

    Setup dynamic fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
         serif: ["Raleway"],
         sans: ['"Playfair Display"'],
       },
+      fontSize: {
+        dynamic: [
+          "clamp(1.5rem, 2.5vw, 2rem)",
+          { lineHeight: "clamp(1.75rem, 2.5vw, 2.5rem)" },
+        ],
+        "dynamic-5xl": [
+          "clamp(3.5rem, 10vw, 8rem)",
+          { lineHeight: "clamp(4rem, 10vw, 9rem)" },
+        ],
+        "dynamic-6xl": [
+          "clamp(5rem, 12vw, 12rem)",
+          { lineHeight: "clamp(4rem, 12vw, 9rem)" },
+        ],
+      },
     },
   },
   plugins: [require("tailwindcss-debug-screens")],
